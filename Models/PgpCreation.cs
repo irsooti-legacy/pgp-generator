75d9b78fafaf4f59d0e0e2795b0601341aeaedfb
Daniele I <irsooti@gmail.com>
2019-02-28 18:01:43 +0100

    alè

---
+++ Models/PgpCreation.cs
@@ -0,0 +1,16 @@
+﻿using Safester.CryptoLibrary.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PgpWeb.App.Models
+{
+    public class PgpCreation
+    {
+        public string Identity { get; set; }
+        public string Passphrase { get; set; }
+        public PublicKeyAlgorithm Algorithm { get; set; }
+        public PublicKeyLength Length { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PgpWeb.App.Models;
+using Safester.CryptoLibrary.Api;
+
+namespace PgpWeb.App.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+        [HttpPost]
+        public void OnPost(PgpCreation model)
+        {
+            try
+            {
+                var generator = new PgpKeyPairGenerator(model.Identity, model.Passphrase.ToCharArray(), model.Algorithm, model.Length);
+                var generatedKey = generator.Generate();
+
+                ViewData["Identity"] = model.Identity;
+                ViewData["Passphrase"] = model.Passphrase;
+                ViewData["Algorithm"] = model.Algorithm;
+                ViewData["Length"] = model.Length;
+
+                ViewData["PrivateKey"] = generatedKey.PrivateKeyRing;
+                ViewData["PublicKey"] = generatedKey.PublicKeyRing;
+            }
+
+            catch (Exception err)
+            {
+                ModelState.AddModelError("generic", err.Message);
+            }
+        }
+    }
+}
